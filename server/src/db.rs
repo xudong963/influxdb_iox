af9d2c6cf9192dea3ce61df8b9ed5b3ab84da4c8
Andrew Lamb <andrew@nerdnetworks.org>
2021-06-01 17:40:53 -0400

    test: showing predicates not working correctly

--- server/src/db.rs
+++ server/src/db.rs
@@ -2041,6 +2041,80 @@ mod tests {
     }
 
     #[tokio::test]
+    async fn read_with_predicates() {
+        // Test that partition pruning is connected up
+        let test_db = make_db().await;
+        let db = Arc::new(test_db.db);
+
+        // Chunk 0 has bar:[1-2]
+        write_lp(db.as_ref(), "cpu bar=1 10");
+        write_lp(db.as_ref(), "cpu bar=2 20");
+
+        let partition_key = "1970-01-01T00";
+        let mb_chunk = db
+            .rollover_partition(partition_key, "cpu")
+            .await
+            .unwrap()
+            .unwrap();
+        db.load_chunk_to_read_buffer(partition_key, "cpu", mb_chunk.id(), &Default::default())
+            .await
+            .unwrap();
+
+        // Chunk 1 has bar:[3-3] (going to get pruned)
+        write_lp(db.as_ref(), "cpu bar=3 10");
+
+        let partition_key = "1970-01-01T00";
+        let mb_chunk = db
+            .rollover_partition(partition_key, "cpu")
+            .await
+            .unwrap()
+            .unwrap();
+        db.load_chunk_to_read_buffer(partition_key, "cpu", mb_chunk.id(), &Default::default())
+            .await
+            .unwrap();
+
+        let expected = vec![
+            "+-----+-------------------------------+",
+            "| bar | time                          |",
+            "+-----+-------------------------------+",
+            "| 1   | 1970-01-01 00:00:00.000000010 |",
+            "| 2   | 1970-01-01 00:00:00.000000020 |",
+            "| 3   | 1970-01-01 00:00:00.000000010 |",
+            "+-----+-------------------------------+",
+        ];
+
+        // predicate using no predicate
+        let batches = run_query(Arc::clone(&db), "select * from cpu").await;
+        assert_batches_sorted_eq!(&expected, &batches);
+
+        // predicate using the bar < 3.0 is correct:
+        let batches = run_query(Arc::clone(&db), "select * from cpu where bar < 4.0").await;
+        assert_batches_sorted_eq!(&expected, &batches);
+
+        // predicate using the bar < 4 (not double) is NOT correct:
+        //let batches = run_query(Arc::clone(&db), "select * from cpu where bar < 4").await;
+        //assert_batches_sorted_eq!(&expected, &batches);
+
+        let expected = vec![
+            "+-----+-------------------------------+",
+            "| bar | time                          |",
+            "+-----+-------------------------------+",
+            "| 1   | 1970-01-01 00:00:00.000000010 |",
+            "| 2   | 1970-01-01 00:00:00.000000020 |",
+            "+-----+-------------------------------+",
+        ];
+
+        // predicate using the bar < 3.0 is correct:
+        let batches = run_query(Arc::clone(&db), "select * from cpu where bar < 3.0").await;
+        assert_batches_sorted_eq!(&expected, &batches);
+
+        // predicate using the bar < 3 (not double) is NOT correct:
+        let batches = run_query(Arc::clone(&db), "select * from cpu where bar < 3").await;
+        assert_batches_sorted_eq!(&expected, &batches); // <-- This assertion fails
+    }
+
+
+    #[tokio::test]
     async fn load_to_read_buffer_sorted() {
         let test_db = make_db().await;
         let db = Arc::new(test_db.db);
